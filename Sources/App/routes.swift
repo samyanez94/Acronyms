1c25d5042ff473d8a1272b660ad3ab04fa740188
Samuel Yanez <samuelyanez94@gmail.com>
2020-09-27 12:20:02 -0700

    Add advanced Fluent operations

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,6 +2,9 @@ import Fluent
 import Vapor
 
 func routes(_ app: Application) throws {
+    
+    // MARK: - CRUD Operations
+    
     app.post("api", "acronyms") { req -> EventLoopFuture<Acronym> in
         let acronym = try req.content.decode(Acronym.self)
         return acronym.save(on: req.db).map { acronym }
@@ -35,5 +38,31 @@ func routes(_ app: Application) throws {
                 acronym.delete(on: req.db)
                     .transform(to: .noContent)
             }
+    }
+    
+    // MARK: - Advance Operations
+    
+    app.get("api", "acronyms", "first") { req -> EventLoopFuture<Acronym> in
+        Acronym.query(on: req.db)
+            .first()
+            .unwrap(or: Abort(.notFound))
+    }
+    
+    app.get("api", "acronyms", "sorted") { req -> EventLoopFuture<[Acronym]> in
+        Acronym.query(on: req.db)
+            .sort(\.$short, .ascending)
+            .all()
+    }
+    
+    app.get("api", "acronyms", "search") { req -> EventLoopFuture<[Acronym]> in
+        guard let searchTerm = req.query[String.self, at: "term"] else {
+            throw Abort(.badRequest)
+        }
+        return Acronym.query(on: req.db)
+            .group(.or) { or in
+                or.filter(\.$short == searchTerm)
+                or.filter(\.$long == searchTerm)
+            }
+            .all()
     }
 }
