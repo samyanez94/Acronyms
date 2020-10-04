14a2375d73c4667d4226561475018fcc99858fae
Samuel Yanez <samuelyanez94@gmail.com>
2020-10-03 19:07:14 -0700

    Reorganize file hierarchy

--- Package.swift
+++ Package.swift
@@ -7,7 +7,6 @@ let package = Package(
        .macOS(.v10_15)
     ],
     dependencies: [
-        // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0")
@@ -21,9 +20,6 @@ let package = Package(
                 .product(name: "Vapor", package: "vapor")
             ],
             swiftSettings: [
-                // Enable better optimizations when building in Release configuration. Despite the use of
-                // the `.unsafeFlags` construct required by SwiftPM, this flag is recommended for Release
-                // builds. See <https://github.com/swift-server/guides#building-for-production> for details.
                 .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
             ]
         ),
